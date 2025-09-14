1e438a1a968c773ea37529eff7e8bb3ac5a06d09
hako (ngHieu) <a316nguyenminhhieu@gmail.com>
2025-09-15 01:49:38 +0700

    Update CalendarView.tsx

--- src/views/CalendarView.tsx
+++ src/views/CalendarView.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { getTasksLocal } from "../services/taskLocal";
 import TaskCalendar from "../components/TaskCalendar";
 
