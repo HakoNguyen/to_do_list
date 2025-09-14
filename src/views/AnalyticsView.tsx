34762a53883c05c20b390df25020d5f47e840d2b
hako (ngHieu) <a316nguyenminhhieu@gmail.com>
2025-09-15 01:48:01 +0700

    Update AnalyticsView.tsx

--- src/views/AnalyticsView.tsx
+++ src/views/AnalyticsView.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { getTasksLocal } from "../services/taskLocal";
 import TaskAnalytics from "../components/TaskAnalytics";
 
